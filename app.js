65843af4c3928d353309081f2290c00246f89dc6
Tatiana <tatianaw83@gmail.com>
2020-05-04 13:31:45 -0700

    updated randomized color

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const container = document.querySelector('#container')
-const createButton = document.querySelector('#create')
-const clearButton = document.querySelector('#clear')
+const createButton = document.querySelector('.create')
+const clearButton = document.querySelector('.clear')
 let input = document.querySelector('input')
 
 function createGrid(){
+    if(container.firstElementChild){container.removeChild(container.firstElementChild)}
     columns = input.value
     container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${columns}, 1fr)`;
     container.style.gridGap = '1px'
+    let color = changeColors()
     for (i = 0; i < columns * columns; i++){
         let div = document.createElement('div') 
         div.className = 'gridItem'
@@ -17,7 +19,7 @@ function createGrid(){
     items = document.querySelectorAll('.gridItem')
     items.forEach(item => {
         item.addEventListener('mouseenter', function(e){
-            e.target.style.backgroundColor = 'white'
+            e.target.style.backgroundColor = color
         })
     }
         
@@ -28,11 +30,18 @@ function createGrid(){
 
 function clearGrid(){
     //while firstElementChild exists, remove it
-    while (container.firstElementChild){
-        container.removeChild(container.firstElementChild)
-    }
+    while (container.firstChild){
+        container.removeChild(container.firstChild)
+    } 
 }
 
+function changeColors(){
+    let startingRed = Math.floor(Math.random() * 256);
+    let startingBlue = Math.floor(Math.random() * 256);
+    let startingGreen = Math.floor(Math.random() * 256);
+    let randomColor = `rgb(${startingRed}, ${startingBlue}, ${startingGreen})`
+    return randomColor
+}
 
 createButton.addEventListener('click', function(){
     createGrid()
