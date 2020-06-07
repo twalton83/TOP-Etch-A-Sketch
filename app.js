86edec9cb8cbfa7d96bb2c00fe1bf6d33e0a0307
Tatiana <tatianaw83@gmail.com>
2020-06-06 18:31:13 -0700

    updated functions

--- app.js
+++ app.js
@@ -2,30 +2,22 @@ const container = document.querySelector('#container')
 const createButton = document.querySelector('.create')
 const clearButton = document.querySelector('.clear')
 let input = document.querySelector('input')
+let color = changeColors()
 
 function createGrid(){
-    if(container.firstElementChild){container.removeChild(container.firstElementChild)}
+    if(container.firstElementChild){while(container.firstElementChild){container.removeChild(container.firstElementChild)}}
     columns = input.value
     container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${columns}, 1fr)`;
     container.style.gridGap = '1px'
-    let color = changeColors()
+    color = changeColors()
     for (i = 0; i < columns * columns; i++){
         let div = document.createElement('div') 
         div.className = 'gridItem'
         div.style.backgroundColor = 'black'
+        div.addEventListener('mouseover', mouseOverColor)
         container.appendChild(div)
     }
-    items = document.querySelectorAll('.gridItem')
-    items.forEach(item => {
-        item.addEventListener('mouseenter', function(e){
-            e.target.style.backgroundColor = color
-        })
-    }
-        
-    );
-    
-    
 }
 
 function clearGrid(){
@@ -33,6 +25,11 @@ function clearGrid(){
     while (container.firstChild){
         container.removeChild(container.firstChild)
     } 
+}
+function mouseOverColor(e){
+  
+    console.log(e.target.style.backgroundColor)
+    e.target.style.backgroundColor = color
 }
 
 function changeColors(){
@@ -43,9 +40,7 @@ function changeColors(){
     return randomColor
 }
 
-createButton.addEventListener('click', function(){
-    createGrid()
-})
+createButton.addEventListener('click', createGrid)
 clearButton.addEventListener('click', function(){
     clearGrid()
 })
